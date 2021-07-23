77e08fac8efb13ec324f17d61dc99862f84c9b49
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2021-07-23 23:00:52 +0100

    Test code clean up

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,17 +3,16 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+let FetchedAccounts;
 
-beforeEach(() => {
+beforeEach(async() => {
     //Get a list of my accounts associated with ganache network
-    web3.eth.getAccounts().then( fetchedAccounts => {
-        console.log(fetchedAccounts);
-    });
+    FetchedAccounts = await web3.eth.getAccounts();
 
 
-    //Use of the accounts to deploy the contract
+    //Use of the fetched accounts to deploy the contract
 });
 
 it('Show all my ganache eth accounts', () => {
-
+    console.log(FetchedAccounts);
 });
