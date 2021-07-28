fb30093f3a1a7c9a8c72a5b3c8beec153e6177c9
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2021-07-29 00:29:41 +0100

    fix issue with compile.js file

--- compile.js
+++ compile.js
@@ -2,7 +2,7 @@ const path = require('path');
 const filesource = require('fs');
 const solc = require('solc');
 
-const inboxContractPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const inboxContractPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
 const sourceCode = filesource.readFileSync(inboxContractPath, 'utf8');
 
-module.exports = solc.compile(sourceCode, 1).contracts[':Inbox'];
+module.exports = solc.compile(sourceCode, 1).contracts[':Lottery'];
