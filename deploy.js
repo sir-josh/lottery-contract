ede3897f1d0bfb48377b924d91e5eb2f1d55b3af
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2021-08-02 00:11:54 +0100

    get smart contract interface

--- deploy.js
+++ deploy.js
@@ -18,6 +18,7 @@ const deployContract = async () => {
                                 .deploy({ data: bytecode })
                                 .send({ gas: '1000000', from: accounts[0]})
 
+    console.log(interface);
     console.log('Contract deployed to ', deploymentResult.options.address);
 }
 
